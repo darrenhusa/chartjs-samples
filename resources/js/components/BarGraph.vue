a345f12ec89db58e13d709219f96b89a0e71feae
Darren Henderson <darrenh@Darrens-MacBook-Pro.local>
2020-07-20 08:22:32 -0500

    initial commit

---
+++ resources/js/components/BarGraph.vue
@@ -0,0 +1,68 @@
+<template>
+  <canvas width="400" height="400"></canvas>
+  <!-- <canvas id="myChart" width="400" height="400"></canvas> -->
+</template>
+
+<script>
+import Chart from 'chart.js';
+
+export default {
+    // props: ['legend'],
+    props: ['legend', 'chartdata', 'color'],
+
+    // data() {
+    //   legend: []
+    //   // chartdata: []
+    // },
+
+    mounted() {
+        console.log('BarGraph component mounted.');
+
+        // var ctx = document.getElementById('myChart');
+        var ctx = this.$el;
+
+        var myChart = new Chart(ctx, {
+        type: 'line',
+        // type: 'bar',
+        data: {
+            // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: this.legend,
+            datasets: [{
+                label: '# of Votes',
+                data: this.chartdata,
+                // backgroundColor: [
+                //     'rgba(255, 99, 132, 0.2)',
+                //     'rgba(54, 162, 235, 0.2)',
+                //     'rgba(255, 206, 86, 0.2)',
+                //     'rgba(75, 192, 192, 0.2)',
+                //     'rgba(153, 102, 255, 0.2)',
+                //     'rgba(255, 159, 64, 0.2)'
+                // ],
+                borderColor: [
+                    this.color
+                    // 'rgba(255, 99, 132, 1)',
+                    // 'rgba(54, 162, 235, 1)',
+                    // 'rgba(255, 206, 86, 1)',
+                    // 'rgba(75, 192, 192, 1)',
+                    // 'rgba(153, 102, 255, 1)',
+                    // 'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+        });
+
+    }
+}
+// import Chart from 'chart.js';
+//
+</script>
---
+++ resources/views/welcome-chart.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Chart.js Practice</title>
+</head>
+<body>
+  <h1>Chart.js Practice</h1>
+  <div id="root">
+    {{-- <example-component></example-component> --}}
+    <bar-graph v-bind:legend="['Jan', 'Feb', 'Mar']"
+               v-bind:chartdata="[20, 10, 30]"
+               color="green"
+    ></bar-graph>
+    <bar-graph v-bind:legend="['01', '02', '03']"
+               v-bind:chartdata="[100, 50, 25]"
+    ></bar-graph>
+
+    {{-- <bar-graph></bar-graph> --}}
+  </div>
+  {{-- <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script> --}}
+  <script src="/js/app.js"></script>
+</body>
+</html>
