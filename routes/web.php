e5ae7e74e07f3d47ff389373fc9d85d688dc70f1
Darren Henderson <darrenh@Darrens-MacBook-Pro.local>
2020-07-20 10:50:32 -0500

    Added an /api/revenue route to return the chart labels and chartdata.

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,24 @@ Route::get('/', function () {
     return view('welcome-chart', compact('labels', 'chartdata'));
 });
 
+Route::get('/api/revenue', function () {
+  $labels = collect(['01', '02', '03']);
+  $chartdata = collect([50, 25, 10]);
+
+  $result = collect([
+    'labels' => $labels,
+    'chartdata' => $chartdata,
+  ]);
+
+  // dd($result);
+
+  return $result;
+
+  // dd($labels, $chartdata);
+
+    // return view('welcome-chart', compact('labels', 'chartdata'));
+});
+
 // Route::get('/revenue', function () {
 //     return view('welcome-chart');
 // });
