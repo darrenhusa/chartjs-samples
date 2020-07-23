ffbb26bd1ec3edf2c87ee38d2b547301820334b9
Darren Henderson <darrenh@Darrens-MacBook-Pro.local>
2020-07-22 21:16:59 -0500

    Added ft_pt_status field as an eloquent accessor.

--- app/Student.php
+++ app/Student.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    public $timestamps = false; 
+    public $timestamps = false;
+
+    public function getFtPtStatusAttribute()
+    {
+      if($this->credit_hours_enrolled >= 12)
+      {
+        $result = 'FT';
+      }
+      else
+      {
+        $result = 'PT';
+
+      }
+      return $result;
+
+      //TODO Convert to ternary form???
+    }
 }
